72bde928deebc32b3b2d3d2be1ce7542d9d2640b
gsavichev <galesavich@gmail.com>
2024-02-13 20:28:18 +0500

    Создан класс Afisha

---
+++ Models/Afisha.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace savichev27pr.Models
+{
+    public class Afisha
+    {
+        public int Id { get; set; }
+        public int IdKinoteatr { get; set; }
+        public string Name { get; set; }
+        public DateTime Time { get; set; }
+        public int Price { get; set; }
+
+        public Afisha(int id, int idKinoteatr, string name, DateTime time, int price)
+        {
+            Id = id;
+            IdKinoteatr = idKinoteatr;
+            Name = name;
+            Time = time;
+            Price = price;
+        }
+    }
+}
