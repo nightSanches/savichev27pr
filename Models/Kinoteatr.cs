4c3bd3dbc4730f825dac3f8bebf9d82007bc2ece
gsavichev <galesavich@gmail.com>
2024-02-13 20:25:49 +0500

    Добавьте файлы проекта.

---
+++ Models/Kinoteatr.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace savichev27pr.Models
+{
+    public class Kinoteatr
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public int CountZal {  get; set; }
+        public int Count { get; set;}
+
+        public Kinoteatr(int id, string name, int countZal, int count)
+        {
+            Id = id;
+            Name = name;
+            CountZal = countZal;
+            Count = count;
+        }
+    }
+}
