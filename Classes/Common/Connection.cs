d8b87c10410be57efd2aa4eea75bae3a9ffbf00b
gsavichev <galesavich@gmail.com>
2024-02-13 20:37:15 +0500

    Разработан класс Connection. Савичев

---
+++ Classes/Common/Connection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace savichev27pr.Classes.Common
+{
+    public class Connection
+    {
+        public static  readonly string config = "server=localhost;uid=root;pwd=;database=kino;";
+        public MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(config);
+            connection.Open();
+            return connection;
+        }
+
+        public MySqlDataReader Queryable(string SQL, MySqlConnection connection)
+        {
+            return new MySqlCommand(SQL, connection).ExecuteReader();
+        }
+
+        public void CloseConnection(MySqlConnection connection) 
+        { 
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+        }
+    }
+}
